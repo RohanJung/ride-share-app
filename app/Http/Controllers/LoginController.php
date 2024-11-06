05d22e14e56aa4136c26604b1bcc67ce9e032102
RohanJung <rohanjungtmg@gmail.com>
2024-11-06 20:49:37 +0545

    logincontroller sumbit

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -6,5 +6,21 @@ use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
-    //
+    public function Sumbit(Request $request){
+        $request->validate([
+            'phone' => 'required|numeric|min:10',
+        ]);
+
+        $user = User::firstOrCreate([
+            'phone' => $request->phone,
+        ]); 
+
+        if(!$user){
+            return response()->json([
+                'message' => 'User not found',
+            ], 404);    
+        }
+
+        $user->notify();
+    }
 }
