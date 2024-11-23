5c29698a45042f72560dd7b025f6e951564d448a
RohanJung <rohanjungtmg@gmail.com>
2024-11-23 22:20:29 +0545

    trip controller and route

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,12 @@ use App\Http\Controllers\LoginController;
 
 Route::post('/login',[LoginController::class,'Submit']);
 Route::post('/login/verify',[LoginController::class,'Verify']);
+
+
+Route::get('/driver',[DriverController::class,'show']);
+Route::post('/driver',[DriverController::class,'update']);
+
+Route::get('/user', function(Request $request){
+    return $request->user();
+});
+
