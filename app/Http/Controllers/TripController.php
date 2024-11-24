cd2bbe9713d05575d31fbd0367a6202d9d0e485b
RohanJung <rohanjungtmg@gmail.com>
2024-11-24 21:24:58 +0545

    trip controller store and show method

--- app/Http/Controllers/TripController.php
+++ app/Http/Controllers/TripController.php
@@ -6,5 +6,28 @@ use Illuminate\Http\Request;
 
 class TripController extends Controller
 {
-    //
+    public function store(Request $request){
+        $rquest->validate([
+            'origin'=>'required',
+            'destination'=>'required',
+            'destination_name'=>'required',
+        ]);
+
+        return $request->user()->trips()->create([
+            'origin',
+            'destination',
+            'destination_name',
+        ]);
+    }
+
+    public function show(Request $request,Trip $trip){
+        if($trip->user->id === $request->user()->id){
+            return $trip;
+        }
+
+        if($trip->driver->id === $request->user()->driver->id){
+            return $trip;
+        }
+        return response()->json(['error'=>'Canot find the trip'],404); 
+    }
 }
